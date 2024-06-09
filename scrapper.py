7ecd78ab4e0a4b75508ebc62051b92cb67ab5988
Rustam Zokirov <zokirovrustam202@gmail.com>
2024-06-09 14:46:41 +0500

    Refresh repo

--- scrapper.py
+++ scrapper.py
@@ -10,6 +10,13 @@ NOTEs:
 10K problem solved, input playlist can have only 10K, so output will also have 10K.
 
 # ONLY Works with 100 songs, because scrapping loads only 100 songs
+
+url_1 = "https://music.yandex.com/users/r4khimov.sardor/playlists/3?utm_medium=copy_link"
+url_2 = "https://music.yandex.com/users/akarimovr01@gmail.com/playlists/3"
+url_3 = "https://music.yandex.com/users/zokirovrustam202@gmail.com/playlists/3"
+# Khvangene
+# asadbek1khasanov@gmail.com
+
 """
 import requests
 from bs4 import BeautifulSoup
