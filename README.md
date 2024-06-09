b2e75b3338b24dff3908b178df035b52ed5ec5ab
Rustam Zokirov <zokirovrustam202@gmail.com>
2024-06-09 23:47:52 +0500

    Refactoring handlers

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
-# Yandex Music -> Spotify
-Перенос плейлистов Yandex Music в Spotify.
+# Yandex.Music to Spotify
+Easily move Yandex Music playlist songs to Spotify.
 
-## DO not forget to update the Access Token!
-- https://developer.spotify.com/dashboard/login
+## How to use?
+- Create app on Spotify and get the client id and client secret.
+- Create `.env` file. Copy the content from `.env.example` and update the values.
+  - To get Spotify Access Token, follow the instructions here https://developer.spotify.com/dashboard/login.
--- scrapper.py
+++ scrapper.py
@@ -1,131 +1,143 @@
-"""
-Input: Gets as the link of the playlist from Yandex Music
-like (https://music.yandex.com/users/zokirovrustam202@gmail.com/playlists/3).
-Then using the web scrapper, get all song names from the playlist.
+import csv
+import json
+import os
+from typing import Optional
 
-Output: Data is saved in CSV  (FOR Prototype #1, use Kafka soon, if we want the synchronization,
-between the accounts.)
-
-NOTEs:
-10K problem solved, input playlist can have only 10K, so output will also have 10K.
-
-# ONLY Works with 100 songs, because scrapping loads only 100 songs
-
-url_1 = "https://music.yandex.com/users/r4khimov.sardor/playlists/3?utm_medium=copy_link"
-url_2 = "https://music.yandex.com/users/akarimovr01@gmail.com/playlists/3"
-url_3 = "https://music.yandex.com/users/zokirovrustam202@gmail.com/playlists/3"
-# Khvangene
-# asadbek1khasanov@gmail.com
-
-"""
 import requests
 from bs4 import BeautifulSoup
-import json
-import csv
-import os
-from threading import Thread
+from decouple import config
 
 
-class Scrapper:
-    user_id = "314vqpqyrdpvs5uyer22bbusxcoq"
-    counter = 0
-    token = "BQACxRP4AtgWSDj0Dpn0gAcUG0adiOEm5HeDKWrn2NJGBeqidBJXEuh76UK64ChtFwUYbf4mJdSYt4vkCnGne3DyK6x19CBx4AemAZ-EIhJw1HhstcsSjvtCjtq_1Glil7kgu5eGisPXLDSOGBG3KNPuQIkJBrOfMW4TGDq1jVxVt7xOqohP9qbROXy1mpFHWf8w2k_v2WI8UIqS62PZeAuVrlc352Ne"
-    number_of_songs = 0
-    uris = []
-
+class YandexMusicHandler:
     def __init__(self, playlist_url: str):
         self.playlist_url = playlist_url
 
-    def get_page_content(self):
-        page = requests.get(self.playlist_url)
-        return page
-
     def get_playlist_songs(self):
-        page = self.get_page_content()
-        soup = BeautifulSoup(page.content, "html.parser")
-        results = soup.find_all("div", class_="d-track__overflowable-column")
+        ...
 
-        # Remove data.csv before appending to it
-        file = 'data.csv'
+    def save_playlist_into_file(self, file=".data.csv") -> str:
         if os.path.exists(file) and os.path.isfile(file):
             os.remove(file)
 
-        # Kafka
+        html_page = self._get_page_content()
+        html_parser = BeautifulSoup(html_page.content, "html.parser")
+        results = html_parser.find_all("div", class_="d-track__overflowable-column")
+
+        number_of_songs = 0
         for result in results:
             title_element = result.find("div", class_="d-track__name").text.strip()
             author_element = result.find("span", class_="d-track__artists").text.strip()
-            self.number_of_songs += 1
+            number_of_songs += 1
 
-            with open(file, 'a') as f:
+            with open(file, "a") as f:
                 writer = csv.writer(f)
                 writer.writerow([title_element, author_element])
-        # print(self.number_of_songs)
 
-    def create_spotify_playlist(self):
-        endpoint_url = f"https://api.spotify.com/v1/users/{self.user_id}/playlists"
-        request_body = json.dumps({
-            "name": f"Playlist #{self.playlist_url}",
-            "description": "Welcome to playlist generated with programming code!",
-            "public": True
-        })
-        response = requests.post(url=endpoint_url, data=request_body,
-                                 headers={"Content-Type": "application/json",
-                                          "Authorization": f"Bearer {self.token}"})
+        return f"Number of processed songs: {number_of_songs}. File: {file}"
 
-        playlist_id = response.json()['id']
-        url = response.json()['external_urls']['spotify']  # Spotify playlist URL
+    def _get_page_content(self):
+        page = requests.get(self.playlist_url)
+        return page
+
+
+class SpotifyHandler:
+    def __init__(
+        self,
+        client_id: str,
+        client_secret: str,
+        api_host: Optional[str] = "https://api.spotify.com/v1",
+    ):
+        self.api_host = api_host
+        self.client_id = client_id
+        self.client_secret = client_secret
+        self.uris = []
+
+        # TMP
+        self.token = config("SPOTIFY_TOKEN")
+        self.user_id = config("SPOTIFY_USER_ID")
+
+    def get_access_token(self):
+        ...
+
+    def get_user_id(self):
+        ...
+
+    def create_playlist(self):
+        endpoint_url = f"{self.api_host}/users/{self.user_id}/playlists"
+        request_body = {
+            "name": f"Playlist #{os.urandom(25).hex()}",
+            "public": True,
+        }
+        response = requests.post(
+            url=endpoint_url,
+            json=request_body,
+            headers={
+                "Content-Type": "application/json",
+                "Authorization": f"Bearer {self.token}",
+            },
+        )
+
+        playlist_id = response.json()["id"]
+        url = response.json()["external_urls"]["spotify"]  # Spotify playlist URL
 
         return playlist_id, url
 
-    def get_songs_uri(self):
+    def search_songs(self):
         endpoint_url = f"https://api.spotify.com/v1/search"
 
-        with open("data.csv", 'r') as f:
+        with open("data.csv", "r") as f:
             reader = csv.reader(f)
             for row in reader:
                 track = row[0]
                 artist = row[1]
                 response = requests.get(
                     endpoint_url,
-                    headers={"Content-Type": "application/json",
-                             "Authorization": f"Bearer {self.token}"},
-                    params={'q': track, 'type': 'track', 'limit': 1})
+                    headers={
+                        "Content-Type": "application/json",
+                        "Authorization": f"Bearer {self.token}",
+                    },
+                    params={"q": track, "type": "track", "limit": 1},
+                )
 
                 json_response = response.json()
                 print(json_response)  # if 404 error, then The access token expired
                 arr = json_response["tracks"]["items"]
                 for item in arr:
-                    uri = item['uri']
+                    uri = item["uri"]
                     self.uris.append(uri)
 
         # print(self.uris)  # Need to be done Kafka, start using the content of list, even appending is not finished
 
-    def add_tracks_to_playlist(self):
-        playlist_info = self.create_spotify_playlist()
+    def add_songs_to_playlist(self):
+        playlist_info = self.create_playlist()
 
         spotify_playlist_id, spotify_playlist_url = playlist_info[0], playlist_info[1]
         print(spotify_playlist_id, spotify_playlist_url, self.uris)
 
-        endpoint_url = f"https://api.spotify.com/v1/playlists/{spotify_playlist_id}/tracks"
+        endpoint_url = (
+            f"https://api.spotify.com/v1/playlists/{spotify_playlist_id}/tracks"
+        )
 
-        request_body = json.dumps({
-            "uris": self.uris
-        })
-        response = requests.post(url=endpoint_url, data=request_body, headers={"Content-Type": "application/json",
-                                                                               "Authorization": f"Bearer {self.token}"})
+        request_body = json.dumps({"uris": self.uris})
+        response = requests.post(
+            url=endpoint_url,
+            data=request_body,
+            headers={
+                "Content-Type": "application/json",
+                "Authorization": f"Bearer {self.token}",
+            },
+        )
 
         print(response.status_code)
-        print(f'Your playlist is ready at {spotify_playlist_url}')
+        print(f"Your playlist is ready at {spotify_playlist_url}")
 
 
-if __name__ == '__main__':
-    playlist_url_big = "https://music.yandex.com/users/asadbek1khasanov@gmail.com/playlists/3"
-    playlist_url = "https://music.yandex.com/users/asadbek1khasanov@gmail.com/playlists/1001"
+if __name__ == "__main__":
+    _playlist_url = "https://music.yandex.com/users/zokirovrustam202@gmail.com/playlists/3"
+    yandex_music = YandexMusicHandler(_playlist_url)
+    yandex_music.save_playlist_into_file()
 
-    scrapper = Scrapper(playlist_url_big)
-    scrapper.get_playlist_songs()
-    scrapper.get_songs_uri()
-    scrapper.add_tracks_to_playlist()
-    #     Thread(target = func1).start()
-    #     Thread(target = func2).start()
-
+    # scrapper = Scrapper(_playlist_url)
+    # scrapper.get_playlist_songs()
+    # scrapper.get_songs_uri()
+    # scrapper.add_tracks_to_playlist()
